745cfb27df1932f7fa6e87ec74af8b7b2ad15194
Кирилл <112952627+motorollainlinux@users.noreply.github.com>
2023-09-23 22:10:31 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
-#include <iostream>
+#include <map>
 #include <ctime>
 #include <fstream>
+#include <iostream>
 
 using std::cout;
 using std::cin;
 using std::string;
+using std::map;
+using std::ifstream;
+using std::fstream;
 
 class Pizzeria {
     public:
@@ -81,8 +85,65 @@ class Person_Order {
         this->Order_Date = Order_Date;
     }
 };
-
+int PizzeriaFilling(map<int, Pizzeria> &MapPizzeria) {
+    fstream File_pizzeria("Pizzeria.txt");
+    long unsigned int ID;
+    string name;
+    float rating;
+    int i = 0
+    for(;!File_pizzeria.eof(); i++) {
+        File_pizzeria >> name;
+        File_pizzeria >> ID;
+        File_pizzeria >> rating;
+        Pizzeria pizzeria(ID, name, rating);
+        MapPizzeria.emplace(i, pizzeria);
+    }
+    return i;
+}
+int MenuFilling(map<int, Pizzeria> &MapMenu) {
+    fstream File_menu("Menu.txt");
+    long unsigned int ID;
+    long unsigned int Pizzeria_ID;
+    string Name_pizza;
+    int Price;
+    int i = 0
+    for(;!File_menu.eof(); i++) {
+        File_menu >> ID;
+        File_menu >> Pizzeria_ID;
+        File_menu >> Name_pizza;
+        File_menu >> Price;
+        Menu menu(ID, Pizzeria_ID, Name_pizza, Price);
+        MapMenu.emplace(i, menu);
+    }
+    return i;
+}
+int PersonFilling(map<int, Pizzeria> &MapPerson) {
+    fstream File_person("Persons.txt");
+    long unsigned int ID;
+    string name, address;
+    int age;
+    bool gender;
+    int i = 0
+    for(;!File_pizzeria.eof(); i++) {
+        File_person >> ID;
+        File_person >> name;
+        File_person >> age;
+        File_person >> gender;
+        File_person >> address;
+        Person person(ID, name, age, gender, address);
+        MapPerson.emplace(i, person);
+    }
+    return i;
+}
 int main() {
-    Pizzeria Dodo = Pizzeria(3859231283972471, "Dodo pizza", 4.7);
+    map<int, Pizzeria> MapPizzeria;
+    map<int, Menu> MapMenu;
+    map<int, Person> MapPerson;
+    int MaxPizzeriaMap, MaxMenuMap, MaxPersonMap;
+    MaxPizzeriaMap = PizzeriaFilling(MapPizzeria);
+    MaxMenuMap = MenuFilling(MapMenu);
+    MaxPersonMap = PersonFilling(MapPerson);
+    
     return 0;
 }
+
